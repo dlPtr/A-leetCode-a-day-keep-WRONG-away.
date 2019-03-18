b504b5977871fc24acee38844ee3a7c014ba5160
dlPtr <xxn.dlptr@gmail.com>
2019-03-18 22:16:52 +0800

    modified to c++

--- src/1_Two_Sum.cpp
+++ src/1_Two_Sum.cpp
@@ -1,18 +1,18 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target) {
-    int * pRet = (int *)malloc(sizeof(int) * 2);
-    
-    for (int i = 0; i < numsSize - 1; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
-                pRet[0] = i;
-                pRet[1] = j;
-                break;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.find(target - nums[i]) != hash.end()) {
+                result.push_back(hash[target - nums[i]]);
+                result.push_back(i);
+                return result;
             }
+            hash[nums[i]] = i;
         }
+        
+        return result;
     }
-    
-    return pRet;
-}
+};
