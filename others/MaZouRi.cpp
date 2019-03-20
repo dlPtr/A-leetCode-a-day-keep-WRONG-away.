51d487d9d1f201e3d20f32d8488f650aea8918f1
dlPtr <xxn.dlptr@gmail.com>
2019-03-20 22:11:10 +0800

    马走日算法

---
+++ others/MaZouRi.cpp
@@ -0,0 +1,65 @@
+/*
+ * 马走日算法
+ *
+ *  Created on: 2019年3月20日
+ *      Author: 17500
+ */
+#include <iostream>
+#include <cstring>
+
+using std::cin;
+using std::cout;
+
+int m, n;
+int way;
+
+int x_add[8] = {2, 2, 1, -1, -2, -2, 1, -1};
+int y_add[8] = {1, -1, 2, 2, 1, -1, -2, -2};
+
+bool ifLegal(int x, int y)
+{
+	return !(x < 0 || y < 0 || x >= m || y >= n);
+}
+
+void dfs(int x, int y, int cnt, bool matrix[10][10])
+{
+	/* 不可访问 */
+	if(!ifLegal(x, y) || matrix[x][y]) {
+		return;
+	}
+
+	/* 标记为已访问 */
+	cnt++;
+	matrix[x][y] = true;
+
+	/* 判断是否全部访问到 */
+	if ((m * n) == cnt) {
+		way++;
+		return;
+	}
+
+	/* 继续深度遍历 */
+	for (int i = 0; i < 8; i++) {
+		bool tmp = matrix[x + x_add[i]][y + y_add[i]];
+		dfs(x + x_add[i], y + y_add[i], cnt, matrix);
+		matrix[x + x_add[i]][y + y_add[i]] = tmp;
+	}
+}
+
+int main()
+{
+	int N;
+	cin>>N;
+	for (int i = 0; i < N; i++) {
+		int x, y;
+		int cnt = 0;
+		bool matrix[10][10] = {{false}};
+
+		cin>>m>>n>>x>>y;
+
+		dfs(x, y, cnt, matrix);
+		printf("ways:%d\n", way);
+	}
+
+	return 0;
+}
