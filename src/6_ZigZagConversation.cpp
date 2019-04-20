b969f37f9d81ab91568639be30d4dcd07d6d5039
dlPtr <xxn.dlptr@gmail.com>
2019-04-20 16:17:15 +0800

    Yeah

--- src/6_ZigZagConversation.cpp
+++ src/6_ZigZagConversation.cpp
@@ -1,11 +1,29 @@
+/*
+ * 思路:(把zig zag画成V形状，然后遍历行，每次处理一行所有的字符)
+ * X     X     X
+ *  P   S R   X X
+ *   X X   X X   X
+ *    X     X
+ * 1、确定步长(步长即跳到同一行下一个对称点或者重复点的距离)
+ *     1.1 对称点(P 的对称点为 S)步长为 stepSymme  = 2 * (numRows - i).
+ *     1.2 重复点(P 的重复点为 R)步长为 stepRepeat = 2 * (numRows - 1).
+ * 2、找到这一行对应的字符
+ *     2.1 根据步长算出各点位置
+ *     2.2 判断这个位置上的字符是否有效
+ *         2.2.1 是否为V的头和尾
+ *         2.2.2 这个点是否有字符
+ * 3、加入 retStr
+ */
 class Solution {
 public:
     string convert(string src, int numRows) {
         int srcLen = src.length();
 
+        /* Actually I'm thinking we shoule return what when para is illegal! */
         if (0 == srcLen || numRows <= 0) {
             return src;
         }
+
         if (srcLen <= 2 || numRows == 1) {
             return src;
         }
