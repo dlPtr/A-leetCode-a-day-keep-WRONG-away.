0f91ddfc5a7ca38583bf9f71156f1c4a6251d5bb
dlPtr <xxn.dlptr@gmail.com>
2020-01-11 01:17:58 +0800

    [ADD] Provide a Makefile and some scripts

---
+++ src/11_Container_With_Most_Water.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        for (vector<int>::iterator it = height.begin(); it != height.end(); it++) {
+            cout << *(it) << endl;
+        }
+
+        return 0;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    const int arr[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    vector<int> input(arr, arr + sizeof(arr) / sizeof(int));
+    Solution demo;
+
+    cout << demo.maxArea(input) << endl;
+
+    return 0;
+}
